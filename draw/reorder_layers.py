75836cb2ca089fc70e4fa0f251da6510285dc29d
r4zendev <reactionnnnn@gmail.com>
2026-01-31 22:24:24 +0100

    ditch qwerty in favor of Vestnik layout for cyrillic languages

--- draw/reorder_layers.py
+++ draw/reorder_layers.py
@@ -1,12 +1,24 @@
-"""Reorder layers in a keymap-drawer YAML file."""
+"""Reorder layers and relabel Vestnik Cyrillic in a keymap-drawer YAML file."""
 import sys
 import yaml
+
+VESTNIK_MAP = {
+    "W": "Ц", "L": "Д", "H": "Р", "U": "Г", "[": "Х",
+    "A": "Ф", "G": "П", "F": "А", "Z": "Я", "'": "Э",
+    "C": "С", "N": "Т", "Y": "Н", "R": "К", ",": "Б",
+    "M": "Ь", "D": "В", "J": "О", "T": "Е", "B": "И",
+    "I": "Ш", "P": "З", "K": "Л", "V": "М", "X": "Ч",
+    ";": "Ж", "Q": "Й", "S": "Ы", "E": "У", ".": "Ю",
+}
 
 path, *order = sys.argv[1:]
 with open(path) as f:
     data = yaml.safe_load(f)
 
 layers = data["layers"]
+if "Vestnik" in layers:
+    layers["Vestnik"] = [VESTNIK_MAP.get(k, k) if isinstance(k, str) else k for k in layers["Vestnik"]]
+
 data["layers"] = {name: layers[name] for name in order if name in layers}
 
 with open(path, "w") as f:
